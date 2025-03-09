4e353a1b16f3aa044e92e3f471ae620e8bc54e8e
Negin <neginmotaharifar79@gmail.com>
2025-03-09 15:14:58 +0100

    Creating a collection of objects with constructor functions

--- meme.mjs
+++ meme.mjs
@@ -33,4 +33,29 @@ function Game(id) {
       return this.score;
     };
   }
+
+  function Collection() {
+    this.items = [];
+  
+    // Add new item
+    this.add = function (item) {
+      this.items.push(item);
+    };
+  
+    // Find item by ID
+    this.findById = function (id) {
+      return this.items.find(item => item.id === id);
+    };
+  
+    // Delete an item by ID
+    this.remove = function (id) {
+      this.items = this.items.filter(item => item.id !== id);
+    };
+  
+    // Get all items
+    this.getAll = function () {
+      return this.items;
+    };
+  }
+  
   
